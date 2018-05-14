569a16359a52d459f1e7f6a61694f72226ec2ca4
Oleg Timoshenko <too.burn@gmail.com>
2018-05-14 23:53:08 +0300

    Bla-bla

--- src/app/interfaces/project.ts
+++
@@ -1,9 +0,0 @@
-export class Project {
-    name: string;
-    description: string;
-    duration: string;
-    teamSize: number;
-    techStack: string;
-
-    constructor() {}
-}
--- src/typings.d.ts
+++
@@ -1,6 +0,0 @@
-/* SystemJS module definition */
-declare const $sf: any;
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
